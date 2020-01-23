6f12cc5dae93cabb508d7ab4e5bfc87d4369d5b5
Niklaus Hirt <niklaushirt@gmail.com>
2020-01-23 08:54:08 +0100

    new structure

--- APPS/devops/install_ARGO.sh
+++ APPS/devops/install_ARGO.sh
@@ -3,6 +3,11 @@ oc create namespace argocd
 # Apply the ArgoCD Install Manifest
 oc -n argocd apply -f https://raw.githubusercontent.com/argoproj/argo-cd/v1.3.6/manifests/install.yaml
 
+
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl delete -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+
 kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server -o name | cut -d'/' -f 2
 
 PATCH='{"spec":{"template":{"spec":{"$setElementOrder/containers":[{"name":"argocd-server"}],"containers":[{"command":["argocd-server","--insecure","--staticassets","/shared/app"],"name":"argocd-server"}]}}}}'
@@ -30,9 +35,24 @@ argocd --insecure --grpc-web login ${ARGOCD_ROUTE}:443 --username admin --passwo
 argocd account update-password --insecure --grpc-web --server ${ARGOCD_ROUTE}:443 --current-password ${ARGOCD_SERVER_PASSWORD} --new-password passw0rd
 
 
+argocd --insecure --grpc-web login ${ARGOCD_ROUTE}:30517 --username admin --password argocd-server-6b8df9749-zdxrc
+
+
+argocd app create guestbook \
+  --repo https://github.com/argoproj/argocd-example-apps.git \
+  --path guestbook \
+  --dest-server https://kubernetes.default.svc \
+  --dest-namespace default
+
+
+argocd repo add https://github.com/argoproj/argocd-example-apps.git
+
 
 
 kubectl delete -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 
+argocd-server-6995fb4f5d-8rxkd
 
+
+Unable to connect repository: unexpected client error: unexpected requesting "https://github.com/argoproj/argocd-example-apps.git/info/refs?service=git-upload-pack" status code: 302
