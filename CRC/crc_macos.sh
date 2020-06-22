bb1933a270fd02f1667cb472ea712da63783091c
Niklaus Hirt <niklaushirt@gmail.com>
2020-06-22 11:42:05 +0200

    crc

--- CRC/crc_macos.sh
+++ CRC/crc_macos.sh
@@ -4,6 +4,24 @@ scp IBM/3_RHOCP/crc-linux-amd64.tar.xz root@192.168.27.180:/home/demo
 scp IBM/3_RHOCP/pull-secret root@192.168.27.180:/home/demo
 
 
+
+scp ./IBM/3_RHOCP/crc-linux-amd64.tar.xz root@192.168.27.111:/root
+
+scp ./Documents/pull-secret root@192.168.27.111:/root
+
+tar xf crc-linux-amd64.tar.xz
+chmod +x crc-linux-1.5.0-amd64/crc
+mv crc-linux-1.5.0-amd64/crc /usr/local/bin/
+
+crc config set pull-secret-file ./pull-secret
+crc config set memory 22800
+crc config set cpus 7
+crc config view
+
+crc setup
+crc start --log-level debug
+
+
 tar xf crc-macos-amd64.tar.xz
 
 sudo mv crc-macos-1.4.0-amd64/crc /usr/local/bin
@@ -12,14 +30,14 @@ sudo mv crc-macos-1.4.0-amd64/crc /usr/local/bin
 crc config view
 
 crc config set pull-secret-file /Users/nhirt/Documents/pull-secret
-crc config set memory 16800
+crc config set memory 32800
 crc config set cpus 12
 
 crc config view
 
 crc setup
 chmod 0600 /etc/hosts
-crc start
+crc start --log-level debug
 
 crc stop
 
@@ -43,3 +61,28 @@ scutil --dns
 dig google.com
 dns-sd -G v4 images.apple.com
 nslookup testing
+
+
+
+ssh -i ~/.crc/cache/crc_libvirt_4.1.11/id_rsa_crc core@192.168.130.11
+sudo -i
+
+
+
+
+crc delete
+cd ~/.crc/bin
+rm hyperkit
+curl -L -O https://686-55985023-gh.circle-artifacts.com/0/hyperkit
+chmod +x hyperkit
+sudo chown root:wheel hyperkit 
+sudo chmod u+s hyperkit
+./hyperkit -version
+
+crc start --log-level debug
+
+ps -Af | grep hyperkit
+sudo vmnet
+
+
+
